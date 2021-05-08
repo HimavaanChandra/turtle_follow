c948a9dc643476f04f0cebfb710e4b1729cb6c91
HimavaanChandra <Himavaan.Chandra@student.uts.edu.au>
2021-05-08 08:35:27 -0700

    working pure pursuit code

--- turtle_follow/src/turtle_follow.cpp
+++ turtle_follow/src/turtle_follow.cpp
@@ -81,26 +81,26 @@ void TurtleFollow::basicController(double centreDistance)
   //Within tolerance
   if (centreDistance >= -0.1 && centreDistance <= 0.1)
   {
-    robot_.control_.linear.x = 0.10;
-    robot_.control_.angular.z = 0;
+    robot_.twist_.linear.x = 0.10;
+    robot_.twist_.angular.z = 0;
   }
   //To the left
   else if (centreDistance < -0.1)
   {
-    robot_.control_.linear.x = 0.11;
-    robot_.control_.angular.z = 0.5;
+    robot_.twist_.linear.x = 0.11;
+    robot_.twist_.angular.z = 0.5;
   }
   //To the right
   else if (centreDistance > 0.1)
   {
-    robot_.control_.linear.x = 0.11;
-    robot_.control_.angular.z = -0.5;
+    robot_.twist_.linear.x = 0.11;
+    robot_.twist_.angular.z = -0.5;
   }
   //Do nothing
   else
   {
-    robot_.control_.linear.x = 0;
-    robot_.control_.angular.z = 0;
+    robot_.twist_.linear.x = 0;
+    robot_.twist_.angular.z = 0;
   }
 }
 
@@ -109,10 +109,25 @@ void TurtleFollow::purePursuit(double centreDistance, double range)
   // Maximum translational velocity	Burger = 0.22 m/s	Waffle = 0.26 m/s
   // Maximum rotational velocity	Burger = 2.84 rad/s (162.72 deg/s)	Waffle = 1.82 rad/s (104.27 deg/s)
   double rotv = 2.84;
-  double gamma = (2 * range * std::sin(centreDistance)) / std::pow(range, 2);
-  double linear_velocity_ = std::sqrt((6 * std::pow(9.81, 2)) / std::abs(gamma));
-  double angular_velocity_ = linear_velocity_ * gamma;
-
+  double gamma = (2 * std::sin(centreDistance)) / std::pow(range, 2);
+  // double linear_velocity_ = std::sqrt((6 * std::pow(9.81, 2)) / std::abs(gamma));
+  double linear_velocity_ = 0.22;
+  double angular_velocity_ = linear_velocity_ * gamma *10;
+  if (gamma < 0)
+  {
+    if (angular_velocity_ < 0)
+    {
+      angular_velocity_ = -angular_velocity_;
+    }
+  }
+  else
+  {
+    if (angular_velocity_ > 0)
+    {
+      angular_velocity_ = -angular_velocity_;
+    }
+  }
+  std::cout << "gamma: " << gamma << " linear: " << linear_velocity_ << " angular: " << angular_velocity_ << std::endl;
   if (linear_velocity_ > robot_.max_vel_)
   {
     linear_velocity_ = robot_.max_vel_;
@@ -124,8 +139,8 @@ void TurtleFollow::purePursuit(double centreDistance, double range)
     linear_velocity_ *= 0.99;
   }
 
-  robot_.control_.linear.x = linear_velocity_;
-  robot_.control_.angular.z = angular_velocity_;
+  robot_.twist_.linear.x = linear_velocity_;
+  robot_.twist_.angular.z = angular_velocity_;
 
   //PMS code
   // double lookahead = 10;
@@ -158,80 +173,80 @@ void TurtleFollow::purePursuit(double centreDistance, double range)
 
 void TurtleFollow::visServo(double centreDistance)
 {
-  // Visual Servoing
-  double linear_velocity_ = 0;
-  double angular_velocity_ = 0;
-  double lambda = 0.01;
-  // 1.93
-  cv::Mat focalLength = [1408.83; 1409.15];
-  cv::Mat prinPoint = [980.52; 521.50];
+  // // Visual Servoing
+  // double linear_velocity_ = 0;
+  // double angular_velocity_ = 0;
+  // double lambda = 0.01;
+  // // 1.93
+  // cv::Mat focalLength = [1408.83; 1409.15];
+  // cv::Mat prinPoint = [980.52; 521.50];
 
-  // Find the centre of the AR tag
-  double x = tag_pose_.position.z;
-  double y = tag_pose_.position.x;
-  double z = tag_pose_.position.y;
-  double Z = 0.2;
-  cv::Mat arPose = [x y z];
-  double oriW = tag_pose_.orientation.w;
-  double oriX = tag_pose_.orientation.x;
-  double oriY = tag_pose_.orientation.y;
-  double oriZ = tag_pose_.orientation.z;
-  tf::Quaternion q(oriX, oriY, OriZ, oriW);
-  tf::Matrix3x3 m(q);
-  double roll, pitch, yaw;
-  m.getRPY(roll, pitch, yaw);
-  // When tag pose x = 0, AR tag is in centre of screen
-  // Tag pose x is y camera
-  // Tag pose z is x camera
-  cv::Mat target = [x; y; Z; 1]; //Might need to change Z to lowercase z if followiing is not working ----------------------------------------
+  // // Find the centre of the AR tag
+  // double x = tag_pose_.position.z;
+  // double y = tag_pose_.position.x;
+  // double z = tag_pose_.position.y;
+  // double Z = 0.2;
+  // cv::Mat arPose = [x y z];
+  // double oriW = tag_pose_.orientation.w;
+  // double oriX = tag_pose_.orientation.x;
+  // double oriY = tag_pose_.orientation.y;
+  // double oriZ = tag_pose_.orientation.z;
+  // tf::Quaternion q(oriX, oriY, OriZ, oriW);
+  // tf::Matrix3x3 m(q);
+  // double roll, pitch, yaw;
+  // m.getRPY(roll, pitch, yaw);
+  // // When tag pose x = 0, AR tag is in centre of screen
+  // // Tag pose x is y camera
+  // // Tag pose z is x camera
+  // cv::Mat target = [x; y; Z; 1]; //Might need to change Z to lowercase z if followiing is not working ----------------------------------------
 
-  // Transform ros ar to camera frame --------------------------------------------------------------------------------------------------------
+  // // Transform ros ar to camera frame --------------------------------------------------------------------------------------------------------
 
-  // Find linear and angular velocity to navigate centre of AR tag to the centre of camera frame using visual servoing
-  cv::Mat imTarget = (target - prinPoint) / focalLength;
-  cv::Mat ar3D = (arPose - prinPoint) / focalLength;
+  // // Find linear and angular velocity to navigate centre of AR tag to the centre of camera frame using visual servoing
+  // cv::Mat imTarget = (target - prinPoint) / focalLength;
+  // cv::Mat ar3D = (arPose - prinPoint) / focalLength;
 
-  // Calculate velocity matrix/feature Jacobian
-  cv::Mat Lxi(2, 6); // Is this how you set the size of a matrix ?? I'm struggling to find an example that I understand --------------------
-  std::vector<cv::Mat> Lx;
-  double n = size(imTarget);
-  for (int i = 0; i < n; i++)
-  {
-    // You are accessing positions that arent defined/set yet --------------------------------------------------------------------------------
-    Lxi[1, 1] = -1 / Z;
-    Lxi[1, 2] = 0;
-    Lxi[1, 3] = x / Z;
-    Lxi[1, 4] = x * y;
-    Lxi[1, 5] = -(1 + x ^ 2);
-    Lxi[1, 6] = y;
+  // // Calculate velocity matrix/feature Jacobian
+  // cv::Mat Lxi(2, 6); // Is this how you set the size of a matrix ?? I'm struggling to find an example that I understand --------------------
+  // std::vector<cv::Mat> Lx;
+  // double n = size(imTarget);
+  // for (int i = 0; i < n; i++)
+  // {
+  //   // You are accessing positions that arent defined/set yet --------------------------------------------------------------------------------
+  //   Lxi[1, 1] = -1 / Z;
+  //   Lxi[1, 2] = 0;
+  //   Lxi[1, 3] = x / Z;
+  //   Lxi[1, 4] = x * y;
+  //   Lxi[1, 5] = -(1 + x ^ 2);
+  //   Lxi[1, 6] = y;
 
-    Lxi[2, 1] = 0;
-    Lxi[2, 2] = -1 / Z;
-    Lxi[2, 3] = y / Z;
-    Lxi[2, 4] = 1 + y ^ 2;
-    Lxi[2, 5] = -x * y;
-    Lxi[2, 6] = -x;
+  //   Lxi[2, 1] = 0;
+  //   Lxi[2, 2] = -1 / Z;
+  //   Lxi[2, 3] = y / Z;
+  //   Lxi[2, 4] = 1 + y ^ 2;
+  //   Lxi[2, 5] = -x * y;
+  //   Lxi[2, 6] = -x;
 
-    Lx.pushback(Lxi);
-  }
+  //   Lx.pushback(Lxi);
+  // }
 
-  // Calculate position error
-  cv::Mat error2 = ar3D - imTarget;
-  std::vector<double> error;
-  error = cv::Mat::reshape(1, 6); // I'm not sure if this is right still tbh ??? -------------------------------------------------------------
-  cv::Mat deltaError = -error * lambda;
+  // // Calculate position error
+  // cv::Mat error2 = ar3D - imTarget;
+  // std::vector<double> error;
+  // error = cv::Mat::reshape(1, 6); // I'm not sure if this is right still tbh ??? -------------------------------------------------------------
+  // cv::Mat deltaError = -error * lambda;
 
-  // Calculate velocity matrix
-  cv::Mat Lx2;
-  cv::Mat Lx2 = pow((Lx.t() * Lx), -1) * Lx.t();
-  cv::Mat velocity;
-  cv::Mat velocity = -lambda * Lx2 * error;
-  double linear_velocity_ = velocity[1, 1];
-  double angular_velocity_ = velocity[2, 1];
+  // // Calculate velocity matrix
+  // cv::Mat Lx2;
+  // cv::Mat Lx2 = pow((Lx.t() * Lx), -1) * Lx.t();
+  // cv::Mat velocity;
+  // cv::Mat velocity = -lambda * Lx2 * error;
+  // double linear_velocity_ = velocity[1, 1];
+  // double angular_velocity_ = velocity[2, 1];
 
-  // Published to ros in robotControl
-  robot_.control_.linear.x = linear_velocity_;
-  robot_.control_.angular.z = angular_velocity_;
+  // // Published to ros in robotControl
+  // robot_.twist_.linear.x = linear_velocity_;
+  // robot_.twist_.angular.z = angular_velocity_;
 }
 
 void TurtleFollow::robotControl()
@@ -243,8 +258,8 @@ void TurtleFollow::robotControl()
     if (tag_)
     {
       std::cout << "Tag Detected" << std::endl;
-      basicController(tag_pose_.position.x);
-      // purePursuit(tag_pose_.position.x, tag_pose_.position.z); //-------------------------------------------
+      // basicController(tag_pose_.position.x);
+      purePursuit(tag_pose_.position.x, tag_pose_.position.z); //-------------------------------------------
     }
     else
     {
